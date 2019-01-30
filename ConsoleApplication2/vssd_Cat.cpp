3460a1c8ba648aae67f78e0737fba59945a2543e
songjiaheng <song8713@qq.com>
2019-01-30 10:05:07 +0800

    1.去github

--- ConsoleApplication2/vssd_Cat.cpp
+++ ConsoleApplication2/vssd_Cat.cpp
@@ -7,7 +7,7 @@ void sjh::vssdCat::vCat(vssd_disk & MyVssd, std::wstring & Rear)
 	sjh::vssd_folder * Srcfolder = vssd_vcmd::v_FindPathForFirst(MyVssd, Rear, a);
 	if (Srcfolder) Srcfolder->PrintContent();
 	else
-	{
+	{ 
 		std::cout << "VSSD ERROR : This folder is not exist! " << std::endl;
 	}
 
